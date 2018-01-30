d9d1eb319831eb5c71da869c6134829f55c6ec16
Amol Venkataraman <abbunini@gmail.com>
2018-01-30 08:39:42 +0530

    remove extra directory

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// HPDF Week 3 app. Created by Amol Venkataraman <abbunini@gmail.com>
+
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
@@ -9,6 +11,7 @@ app.use(morgan('combined'));
 var request = require('sync-request');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Returns a HTML template to get Startpoint and EndPoint.
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'HTML', 'textbox.html'));
 });
@@ -16,7 +19,7 @@ app.get('/', function (req, res) {
 app.post('/answer', function(req, res) {
     var fr = JSON.stringify(req.body.StartPoint)
     var to = JSON.stringify(req.body.EndPoint)
-    var key = 'AIzaSyCSb_fvcY-0NwFZbJzVZdvNjoYb3wO9yzY'
+    var key = 'AIzaSyCSb_fvcY-0NwFZbJzVZdvNjoYb3wO9yzY' // The API key
     url = 'https://maps.googleapis.com/maps/api/directions/json?origin=' + fr + '&destination=' + to + '&key=' + key;
     var res1 = request('GET', url);
     var ans1 = JSON.parse(res1.body);
@@ -24,21 +27,19 @@ app.post('/answer', function(req, res) {
     var duration = route[0].legs[0].duration.text;
     var distance = route[0].legs[0].distance.text;
     var summary = route[0].summary;
-    var step = route[0].legs.steps;
+    var step = route[0].legs[0].steps;
     var steps;
     for (k in step) {
-        //var instruction = step[k]["html_instructions"];
-        //instruction = instruction.replace(/<[^>]*>/g, ""); // regex to remove html tags
+        var instruction = step[k]["html_instructions"];
+        instruction = instruction.replace(/<[^>]*>/g, "");
         var StepDistance = step[k]["distance"].text;
         var StepDuration = step[k]["duration"].text;
         console.log(StepDistance + StepDuration);
         steps += "Step " + k + ": " + " ("+ StepDistance +"/"+ StepDuration+")\n";
     }
-    //var frcoordinates = route[0].legs[0].start_location;
-    //var tocoordinates = route[0].legs[0].end_location;
-
     var result = "Total Duration: " + duration + "\n" + "Total Distance: " + distance + "\n\n" + steps;
-    res.send(result);
+    result = result.replace(/<[^>]*>/g, "");
+    res.send(result);// Send the result
 });
 
 var port = 8080;
